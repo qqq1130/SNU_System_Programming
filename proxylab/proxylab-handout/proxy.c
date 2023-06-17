5fa3e461e37ce8d53753d6a5ee680eb5f6df7021
csh <shyeokchoi@gmail.com>
2023-06-18 04:04:36 +0900

    forwarding headers, forwarding response

--- proxylab/proxylab-handout/proxy.c
+++ proxylab/proxylab-handout/proxy.c
@@ -17,7 +17,7 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 
 int main(int argc, char* argv[]) {
     int listenfd;
-    int *connfdp;
+    int *client_fdp;
 
     socklen_t client_len;
     struct sockaddr client_addr;
@@ -32,44 +32,89 @@ int main(int argc, char* argv[]) {
 
     while(1) {
         client_len = sizeof(client_addr);
-        connfdp = Malloc(sizeof(int));
-        *connfdp = Accept(listenfd, &client_addr, &client_len); //second, third parameter can be NULL?
-        Pthread_create(&tid, NULL, serve, (void *)connfdp);
+        client_fdp = Malloc(sizeof(int));
+        *client_fdp = Accept(listenfd, &client_addr, &client_len); //second, third parameter can be NULL?
+        Pthread_create(&tid, NULL, serve, (void *)client_fdp);
     }
     return 0;
 }
 
-void *serve(void *connfdp) {
-	int connfd = *((int *)connfdp);
+void *serve(void *client_fdp) {
+	int client_fd = *((int *)client_fdp);
 	Pthread_detach(Pthread_self());
-	Free(connfdp);
-	proxy(connfd);
-	Close(connfd);
+	Free(client_fdp);
+	proxy(client_fd);
+	Close(client_fd);
 	return NULL;
 }
 
-void proxy(int connfd) {
+void proxy(int client_fd) {
     rio_t client_rio;
+    rio_t server_rio;
     char buf[MAXBUF];
     char uri[MAXURI];
+    char header[MAX_OBJECT_SIZE];
     char http_method[16];
     char http_version[16];
     char *host;
     char *port;
     char *path;
+    char *temp;
+    int server_fd;
+    int stat_code;
+    int host_header_exists = 0;
+    ssize_t n;
 
-    Rio_readinitb(&client_rio, connfd);
+    Rio_readinitb(&client_rio, client_fd);
     if (!Rio_readlineb(&client_rio, buf, MAXBUF)) {
         return; /* reading client request failed */
     }
     sscanf(buf, "%s %s %s", http_method, uri, http_version);
 
     parse_uri(uri, &host, &port, &path);
+
+    server_fd = Open_clientfd(host, port);
+    Rio_readinitb(&server_rio, server_fd);
+
+    sprintf(buf, "GET /%s HTTP/1.0\r\n", path);
+    Rio_writen(server_fd, buf, strlen(buf));
+
+    /* send headers */
+    while ((n = Rio_readlineb(&client_rio, buf, MAXBUF))) {
+		if (!strncmp(buf, "\r\n", 2)) {
+            break;
+        }
+        if (strstr(buf, "Connection:") == buf) {
+            sprintf(buf, "Connection: close\r\n");
+        } else if (strstr(buf, "Proxy-Connection:") == buf) {
+            sprintf(buf, "Proxy-Connection: close\r\n");
+        } else if (strstr(buf, "Host:") == buf) {
+            host_header_exists = 1;
+        }
+        strcat(header, buf);
+	}
+    if (!host_header_exists) {
+        sprintf(buf, "Host: %s\r\n", host);
+        strcat(header, buf);
+    }
+    strcat(header, "\r\n");
+    Rio_writen(server_fd, header, strlen(header));
+
+    /* receive response */
+    while ((n = Rio_readlineb(&server_rio, buf, MAXBUF))) {
+		Rio_writen(client_fd, buf, n);
+
+		if (!strncmp(buf, "\r\n", 2)) {
+            break;
+        }
+	}
+
+    Close(server_fd);
 }
 
 void parse_uri(char *uri, char **host, char **port, char **path) {
     static const char *http = "http://";
-    const char *default_port = "80";
+    static const char *default_port = "80";
     char *next_ptr;
 
     *host = uri + strlen(http);
